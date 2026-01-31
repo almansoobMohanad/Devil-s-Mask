0aaa647015d486405801b1c2ba432c69a655b991
almansoobMohanad <dr.neotrix@gmail.com>
2026-01-31 21:55:32 +0800

    fixed enemy following player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -5,7 +5,6 @@ const SIGHT_RANGE := 20.0
 const VIEW_ANGLE := 120.0
 @onready var ray = $VisionRayCast
 @onready var vision_area = $VisionArea
-#@onready var path_follow = $PathFollow3D
 
 @onready var debug_line = $DebugLine
 
@@ -43,13 +42,18 @@ func _physics_process(delta: float) -> void:
 	var player = get_player_in_sight()
 	if player and is_in_sight(player):
 		scanning = false
-		ray.look_at(player.global_transform.origin)
-		go_to_player(player.global_transform.origin)
+		# Make the enemy face the player
+		var direction_to_player = (player.global_transform.origin - global_transform.origin).normalized()
+		look_at(global_transform.origin + direction_to_player, Vector3.UP)
+		
+		# Move towards player
+		velocity.x = direction_to_player.x * SPEED
+		velocity.z = direction_to_player.z * SPEED
 	else:
 		if not scanning:
 			# Reset scanning
 			scanning = true
-			scan_angle = ray.rotation.y
+			scan_angle = rad_to_deg(ray.rotation.y)
 			
 		scan_angle += scan_speed * scan_direction * delta
 		if scan_angle > scan_max:
@@ -61,16 +65,15 @@ func _physics_process(delta: float) -> void:
 
 		ray.rotation.y = deg_to_rad(scan_angle)
 
-		# Patrol along path
+		# Stop moving when not tracking player
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
 	
 	attack_timer -= delta
 
 	move_and_slide()
+	
+	# Handle collision damage
 	for i in range(get_slide_collision_count()):
 		var collision = get_slide_collision(i)
 		if collision.get_collider() is CharacterBody3D and collision.get_collider().name == "Player" and attack_timer <= 0.0:
@@ -79,7 +82,6 @@ func _physics_process(delta: float) -> void:
 
 func get_player_in_sight() -> CharacterBody3D:
 	for body in vision_area.get_overlapping_bodies():
-		# print("Overlapping body: ", body.name)
 		if body.name == "Player":
 			return body
 	return null
@@ -87,27 +89,17 @@ func get_player_in_sight() -> CharacterBody3D:
 func is_in_sight(player: CharacterBody3D) -> bool:
 	var to_player = player.global_transform.origin - global_transform.origin
 	if to_player.length() > SIGHT_RANGE:
-		# print("Out of range")
 		return false
 	
-	# field of view check
+	# Field of view check
 	var forward_dir = -global_transform.basis.z
 	var angle_to_player = rad_to_deg(forward_dir.angle_to(to_player.normalized()))
 	if angle_to_player > (VIEW_ANGLE / 2.0):
-		# print("Out of view angle")
 		return false
 
 	ray.target_position = to_local(player.global_transform.origin)
 	ray.force_raycast_update()
-	# print("Raycasting...")
-	# print("Is Colliding: ", ray.is_colliding(), " Collider: ", ray.get_collider())
 	return ray.is_colliding() and ray.get_collider() == player
-
-func go_to_player(player_position: Vector3) -> void:
-	var direction := (player_position - global_transform.origin).normalized()
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
-	move_and_slide()
 
 func take_damage(amount: int) -> void:
 	print("Enemy took ", amount, " damage!")
@@ -120,23 +112,3 @@ func deal_damage() -> int:
 
 func die() -> void:
 	queue_free()
-
-func _on_vision_timer_timeout() -> void:
-	var overlaps = $VisionArea.get_overlapping_bodies()
-	if overlaps.size() > 0:
-		for overlap in overlaps:
-			if overlap.name == "player":
-				var playerPosition = overlap.global_transform.origin
-				$VisionRayCast.look_at(playerPosition, Vector3.UP)
-				$VisionRayCast.force_raycast_update()
-				
-				if $VisionRayCast.is_colliding():
-					var collider = $VisionRayCast.get_collider()
-					if collider.name == "player":
-						$VisionRayCast.debug_shape_custom_color = Color(255,0, 0)
-						print("I SEE YOU")
-					else:
-						$VisionRayCast.debug_shape_custom_color = Color(0, 255, 0)
-						print("FUsdwasK")
-	
-	
