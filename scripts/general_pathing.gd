8701348dfcc27954377cb2bf42517f130ad35d7e
AIHunter109 <walkercpt@gmail.com>
2026-01-31 23:10:10 +0800

    Add path following functionality to enemy and create general pathing script

---
+++ scripts/general_pathing.gd
@@ -0,0 +1,12 @@
+extends AnimatableBody3D
+
+@export var path_follow_path : NodePath
+@onready var path_follow = get_node(path_follow_path)
+
+const SPEED := 10.0
+
+func _physics_process(delta: float) -> void:
+    # Move along the path
+    path_follow.progress += SPEED * delta
+    global_transform.origin = path_follow.global_transform.origin
+    look_at(path_follow.global_transform.origin + path_follow.transform.basis.z, Vector3.UP)
